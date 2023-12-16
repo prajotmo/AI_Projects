e18e8d2512ca10afc1c7e11395f62cbf949a1f22
prajotmo <92202820+prajotmo@users.noreply.github.com>
2023-12-16 13:19:19 +0530

    Update BI-LSTM.py

--- BI-LSTM.py
+++ BI-LSTM.py
@@ -1,8 +1,10 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, Bidirectional, LSTM, Dense
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
 
 # Load and preprocess the sample data
 with open('sample_data.txt', 'r', encoding='utf-8') as file:
@@ -23,8 +25,11 @@ for line in data.split('\n'):
 
 max_sequence_length = max([len(seq) for seq in input_sequences])
 input_sequences = pad_sequences(input_sequences, maxlen=max_sequence_length, padding='pre')
-X, y = input_sequences[:,:-1], input_sequences[:,-1]
+X, y = input_sequences[:, :-1], input_sequences[:, -1]
 y = np.array([y]).T
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Build the model
 model = Sequential()
@@ -37,7 +42,39 @@ model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=
 model.summary()
 
 # Train the model
-model.fit(X, y, epochs=100, verbose=1)
+history = model.fit(X_train, y_train, epochs=100, verbose=1, validation_data=(X_test, y_test))
+
+# Evaluate the model
+loss, accuracy = model.evaluate(X_test, y_test, verbose=0)
+print(f'Test Loss: {loss:.4f}')
+print(f'Test Accuracy: {accuracy:.4f}')
+
+# Plot accuracy and loss graphs
+def plot_metrics(history):
+    # Plot training & validation accuracy values
+    plt.figure(figsize=(12, 4))
+    plt.subplot(1, 2, 1)
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('Model Accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend(['Train', 'Validation'], loc='upper left')
+
+    # Plot training & validation loss values
+    plt.subplot(1, 2, 2)
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend(['Train', 'Validation'], loc='upper left')
+
+    plt.tight_layout()
+    plt.show()
+
+# Call the function to plot accuracy and loss graphs
+plot_metrics(history)
 
 # Function to generate the next word
 def generate_next_word(seed_text, model, tokenizer, max_sequence_length):
